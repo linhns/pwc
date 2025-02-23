c1bc2ef2242acd88fc31a2d9aaddf5c402c47a12
Nguyen Son Linh <linhns59@gmail.com>
2025-02-24 00:33:25 +0800

    Add custom number of generated passwords

--- src/main.js
+++ src/main.js
@@ -23,16 +23,27 @@ const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
 
 const generateBtn = document.getElementById("generate-btn")
 const passwordsDiv = document.getElementById("passwords-div")
+const passwordCountInput = document.getElementById("password-count")
 
 generateBtn.addEventListener("click", () => {
     passwordsDiv.innerHTML = ""
-    const passwords = Array.from({ length: 2 }, () => generatePassword(characters, 15))
+    let passwordCount = +passwordCountInput.value
+    const passwords = Array.from({ length: passwordCount }, () => generatePassword(characters, 15))
     for (const password of passwords) {
         const passwordDiv = document.createElement("div")
         passwordDiv.classList.add("password")
         passwordDiv.classList.add("flex")
         passwordDiv.textContent = password
         passwordsDiv.appendChild(passwordDiv)
+    }
+})
+
+passwordCountInput.addEventListener("input", () => {
+    let value = +passwordCountInput.value
+    if (isNaN(value) || value < 1) {
+        passwordCountInput.value = 1
+    } else if (value > 9) {
+        passwordCountInput.value = 9
     }
 })
 
